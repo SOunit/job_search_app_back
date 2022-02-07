8edb3083a4bc2bd3d4b761305622d45cd82fd19e
SOunit <workbookwork7@gmail.com>
2022-02-06 22:44:38 -0800

    add fetching data from mongo

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const testRoutes = require("./routes/test-routes");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  res.send("hi");
-});
+app.use(cors());
 
-// process.env.PORT come from Heroku
-app.listen(process.env.PORT || 5000);
+app.use("/api/tests", testRoutes);
+
+try {
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r27pb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    )
+    .then(() => {
+      // process.env.PORT come from Heroku
+      app.listen(process.env.PORT || 5000);
+    });
+} catch (err) {
+  console.log(err);
+}
