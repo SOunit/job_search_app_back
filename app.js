01c12f3039e049e4c6208e5157ccc58f7af3681b
SOunit <workbookwork7@gmail.com>
2022-06-21 12:21:45 -0700

    add request file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,7 +21,9 @@ try {
     )
     .then(() => {
       // process.env.PORT come from Heroku
-      app.listen(process.env.PORT || 5000);
+      app.listen(PORT, () => {
+        console.log(`listen on ${PORT}`);
+      });
     });
 } catch (err) {
   console.log(err);
