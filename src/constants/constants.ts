0e820bb069639a062b44e4c973683e8ca794b581
SOunit <workbookwork7@gmail.com>
2022-07-06 13:58:17 -0700

    change env var to string

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,7 @@
-export const JOBS_COLLECTION_NAME = process.env.JOBS_COLLECTION_NAME!;
-export const SKILLS_COLLECTION_NAME = process.env.SKILLS_COLLECTION_NAME!;
-export const USERS_COLLECTION_NAME = process.env.USERS_COLLECTION_NAME!;
 export const DB_CONN_STRING = process.env.DB_CONN_STRING!;
 export const DB_NAME = process.env.DB_NAME!;
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY!;
+
+export const JOBS_COLLECTION_NAME = "jobs";
+export const SKILLS_COLLECTION_NAME = "skills";
+export const USERS_COLLECTION_NAME = "users";
