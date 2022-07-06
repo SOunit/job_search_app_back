04620d6a03f87a24f844764cf4582b7621e59938
SOunit <workbookwork7@gmail.com>
2022-07-06 13:19:37 -0700

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,8 @@ import authRoutes from "./routes/authRoutes";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use("/api/jobs", jobRoutes);
 app.use("/api/skills", skillRoutes);
