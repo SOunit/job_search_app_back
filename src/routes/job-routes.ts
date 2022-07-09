28f1502c651a072819181a189f792521a086260d
SOunit <workbookwork7@gmail.com>
2022-07-09 16:27:01 -0700

    add update job logic

--- src/routes/job-routes.ts
+++ src/routes/job-routes.ts
@@ -4,6 +4,7 @@ import {
   deleteJob,
   getJobById,
   getJobs,
+  updateJob,
 } from "../controllers/job.controller";
 import { authenticateToken } from "../middleware/authenticateToken";
 
@@ -13,5 +14,6 @@ router.get("/", getJobs);
 router.get("/:jobId", getJobById);
 router.post("/", authenticateToken, createJob);
 router.delete("/:jobId", authenticateToken, deleteJob);
+router.patch("/:jobId", authenticateToken, updateJob);
 
 export default router;
