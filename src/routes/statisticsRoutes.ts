31a8dd7feb37326ebc4d6806718a77041d27b84a
SOunit <workbookwork7@gmail.com>
2022-07-25 13:21:38 -0700

    add auth to statistics logic

--- src/routes/statisticsRoutes.ts
+++ src/routes/statisticsRoutes.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import statisticsController from "../controllers/statisticsController";
+import { authenticateToken } from "../middleware/authenticateToken";
 
 const statisticsRoutes = express.Router();
 
-statisticsRoutes.post("/", statisticsController.addSkillsToStatistics);
+statisticsRoutes.post(
+  "/",
+  authenticateToken,
+  statisticsController.addSkillsToStatistics
+);
 statisticsRoutes.get("/:skillId", statisticsController.getStatisticBySkillId);
-statisticsRoutes.post("/delete", statisticsController.removeSkills);
-statisticsRoutes.put("/:jobId", statisticsController.updateSkills);
+statisticsRoutes.post(
+  "/delete",
+  authenticateToken,
+  statisticsController.removeSkills
+);
+statisticsRoutes.put(
+  "/:jobId",
+  authenticateToken,
+  statisticsController.updateSkills
+);
 
 export default statisticsRoutes;
